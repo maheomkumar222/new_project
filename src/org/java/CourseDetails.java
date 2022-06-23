c156787ff803771a00c23481a9194f7f02c91490
maheomkumar222 <maheomkumar222@gmail.com>
2022-06-23 11:49:11 +0530

    Done

---
+++ src/org/java/CourseDetails.java
@@ -0,0 +1,16 @@
+package org.java;
+
+public class CourseDetails {
+private void javaCourse() {
+	System.out.println("java class started");
+}
+private void phythonCourse() {
+	// TODO Auto-generated method stub
+System.out.println("phython class is not started");
+}
+public static void main(String[] args) {
+	CourseDetails c = new CourseDetails();
+	c.javaCourse();
+	c.phythonCourse();
+}
+}
